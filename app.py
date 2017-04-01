ca165777d428c90829cfb09e752a39f43b2e67f2
Yoshimasa Kawano <nanbuwks.office@gmail.com>
2017-04-01 23:03:47 +0900

    loop bug fix

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import time
 import os
 os.system('echo \"none\" > /sys/class/leds/led0/trigger')
 while 1:
-  i=0
-  print("Hello LED is ON "+str(i+1)+"times")
-  for i in range(10):
-    os.system('echo \"255\" > /sys/class/leds/led0/brightness')
-    time.sleep(0.2)
-    os.system('echo \"0\" > /sys/class/leds/led0/brightness')
-    time.sleep(0.2)
-  time.sleep(10)
+  for j in range(10):
+    print("Hello LED is ON "+str(j+1)+"times")
+    for i in range(j):
+      os.system('echo \"255\" > /sys/class/leds/led0/brightness')
+      time.sleep(0.2)
+      os.system('echo \"0\" > /sys/class/leds/led0/brightness')
+      time.sleep(0.2)
+    time.sleep(10)
