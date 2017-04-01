c368fb83651216e2f6a97c7defbacdc5279028a8
Yoshimasa Kawano <nanbuwks.office@gmail.com>
2017-04-01 22:59:33 +0900

    LED Blink increase

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import time
 import os
 os.system('echo \"none\" > /sys/class/leds/led0/trigger')
 while 1:
-  print("Hello LED is ON")
-  os.system('echo \"255\" > /sys/class/leds/led0/brightness')
+  i=0
+  print("Hello LED is ON "+str(i+1)+"times")
+  for i in range(10):
+    os.system('echo \"255\" > /sys/class/leds/led0/brightness')
+    time.sleep(0.2)
+    os.system('echo \"0\" > /sys/class/leds/led0/brightness')
+    time.sleep(0.2)
   time.sleep(10)
-  print("Hello LED is OFF")
-  os.system('echo \"0\" > /sys/class/leds/led0/brightness')
-  time.sleep(10)
-
