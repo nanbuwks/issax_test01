54de93063fad378aa55d4fa79e986df0473f19ae
Yoshimasa Kawano <nanbuwks.office@gmail.com>
2017-04-01 22:54:00 +0900

    change 10 secound toggle

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import time
 import os
 os.system('echo \"none\" > /sys/class/leds/led0/trigger')
 while 1:
+  print("Hello LED is ON")
   os.system('echo \"255\" > /sys/class/leds/led0/brightness')
-  time.sleep(1)
+  time.sleep(10)
+  print("Hello LED is OFF")
   os.system('echo \"0\" > /sys/class/leds/led0/brightness')
-  time.sleep(1)
-  print("Hello IoT from isaax with LED")
+  time.sleep(10)
 
